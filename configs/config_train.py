fed5c7521eace7770844799ccb71d47cc2df0b0b
morpheus9631 <morpheus.hsieh@gmail.com>
2020-09-12 14:06:16 +0800

    commit

--- configs/config_train.py
+++ configs/config_train.py
@@ -4,14 +4,34 @@ from yacs.config import CfgNode as CN
 _C = CN()
 
 _C.WORK = CN()
-_C.WORK.ROOT_PATH = "D:\\GitWork\\dog_breed\\"
-_C.WORK.LABELS_FNAME = "labels.csv"
+_C.WORK.PATH = "D:\\GitWork\\dog_breed\\"
 
 _C.DATA = CN()
-_C.DATA.ROOT_PATH = "D:\\Dataset\\dog_breed\\"
-_C.DATA.TEST_DIR = "test"
-_C.DATA.TRAIN_DIR = "train"
-_C.DATA.FRAC_FOR_TRAIN = 0.8
+_C.DATA.PATH = "D:\\Dataset\\dog_breed\\data\\"
+_C.DATA.CSV_LABELS = "laebls.csv"
+_C.DATA.CSV_SAMPLE_SUBMISSION = "sample_submission.csv"
+_C.DATA.PATH_RAW  = "D:\\Dataset\\dog_breed\\data\\raw\\"
+_C.DATA.DIR_TEST = "test"
+_C.DATA.DIR_TRAIN = "train"
+
+_C.PROCESSED = CN()
+_C.PROCESSED.PATH  = "D:\\Dataset\\dog_breed\\data\\processed\\"
+_C.PROCESSED.CSV_BREEDS = "breeds_processed.csv"
+_C.PROCESSED.CSV_LABELS = "labels_processed.csv"
+_C.PROCESSED.TRAIN_DATA = "train_data.npz"
+_C.PROCESSED.VALID_DATA = "valid_data.npz"
+
+_C.PRETRAINED = CN()
+_C.PRETRAINED.PATH  = "D:\\GitWork\\dog_breed\\models\\"
+_C.PRETRAINED.FNAME = 'resnet50-19c8e357.pth'
+
+_C.TRAIN = CN()
+_C.TRAIN.FRAC_FOR_TRAIN = 0.8
+_C.TRAIN.NUM_BREED_CLASSES = 16
+_C.TRAIN.BATCH_SIZE = 6
+_C.TRAIN.LEARNING_RATE = 0.001
+_C.TRAIN.MOMENTUM = 0.9
+_C.TRAIN.NUM_EPOCHS = 2
 
 
 def get_cfg_defaults():
